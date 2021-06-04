c83f6774df0e95662d6fb273e5fe8a8dc2c38628
ThoreLencer <60389641+ThoreLencer@users.noreply.github.com>
2021-06-04 07:12:18 +0200

    Add files via upload

---
+++ src/define.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <wx/wx.h>
+#include <wx/thread.h>
+
+enum {
+    ID_EVT_CLEARMAILVIEW,
+    ID_EVT_LOGGEDIN,
+    ID_EVT_PERFORMUPDATE,
+    ID_EVT_ONREGISTER,
+    ID_GenKeys,
+    ID_Connect,
+    ID_Registration,
+    ID_Delete,
+    ID_Data_Get,
+    ID_Write_Mail,
+    ID_Delete_Mail,
+    ID_Mail_Folder_Change,
+    UPDATE_THREAD_FINISH,
+    PULSE_PROGRESS,
+    REGISTER_THREAD_FINISH,
+    ID_Edit_IP,
+    ID_Edit_Username,
+    ID_Edit_Email,
+    ID_Edit_Password,
+    ID_Edit_Password_Repeat,
+    ID_Button_Register,
+    ID_Button_PasswdReset,
+    ID_Button_Login,
+    ID_Disconnect,
+    ID_Edit_To,
+    ID_Edit_Caption,
+    ID_Edit_Mail,
+    ID_Button_Send,
+    ID_Timer_Mail_Sync,
+    ID_ListBox_Inbox,
+    ID_Text_Mail
+};
---
+++ src/main.cpp
@@ -0,0 +1,76 @@
+// Copyright 2021 Thore Lencer
+// SPDX-License-Identifier: AGPL-3.0-only
+
+#include <iostream>
+#include <stdio.h>
+#include "define.h"
+#include "GUI/mainFrame.h"
+#include "Encryptor/rsa_encryption.h"
+#include "Database/database.h"
+#include "GUI/login.h"
+#include "GUI/mailwriter.h"
+#include "GUI/register.h"
+#include <mysql/mysql.h>
+
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+    #include <wx/wx.h>
+#endif
+#include <wx/richtext/richtextctrl.h>
+#include <wx/progdlg.h>  
+#include <wx/thread.h>
+#include <wx/dynarray.h>
+#include <wx/htmllbox.h>
+#include <wx/settings.h>
+#include <wx/html/htmlwin.h>
+
+//Library 1: Installing gmp on Linux mingw cross compile for windows 64bit:
+//download gmp library
+//cd into the library folder
+//run: make clean
+//run: export CC=x86_64-w64-mingw32-g++
+//run: export CC_FOR_BUILD=x86_64-linux-gnu-g++
+//run: ./configure --build=x86_64-linux-gnu --host=x86_64-w64-mingw32 --enable-cxx --prefix=/usr/x86_64-w64-mingw32
+//run: make -j8
+//run: make install
+//Library 2: do same for wxWidgets
+//Library 3: mysql port for MINGw : https://github.com/reminisc3/mysql-mingw64-port
+
+//TODO
+//Kontaktbuch
+//Rechtsklick Kontextmenü: Löschen, Antworten, Weiterleiten
+
+class MyApp : public wxApp {
+    wxLocale m_locale;
+public:
+    virtual bool OnInit();
+};
+
+wxIMPLEMENT_APP(MyApp);
+
+void deleteOld(){
+    remove("RSA_Client_Old.exe");
+    remove("RSA_Client_Old");
+}
+
+//Global Vars
+static RSA_Encryptor rsa;
+static Mail_Database database;
+static MyFrame *frame;
+
+bool MyApp::OnInit() {
+    //Delete old Updatefiles
+    deleteOld();
+    //Init Application
+    m_locale.Init(m_locale.GetSystemLanguage(), wxLOCALE_LOAD_DEFAULT);
+    frame = new MyFrame(&database, &rsa);
+    wxSize size;
+    size.x = 800;
+    size.y = 500;
+    frame->SetMinSize(size);
+    frame->SetSize(800, 500);
+    frame->Show(true);
+    database.init();
+    rsa.init();
+    return true;
+}
